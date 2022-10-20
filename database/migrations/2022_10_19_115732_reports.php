23637fbbb1270991e4482365fb0bf72d2e4fc3ea
dovhopolyi-do/56-rEb_G3Kh-Nm90 <dovhopolyi_do@groupbwt.com>
2022-10-20 11:00:24 +0300

    Implemented CRUD functionality for reports

---
+++ database/migrations/2022_10_19_115732_reports.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('topic');
+            $table->timestamp('dateStart');
+            $table->timestamp('dateEnd');
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\ConferenceController;
+use App\Http\Controllers\Api\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -17,18 +18,25 @@ use Illuminate\Support\Facades\Auth;
 */
 
 // Public routes
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/conferences', [ConferenceController::class, 'index']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/conferences', [ConferenceController::class, 'index']);
 Route::get('/conferences/{id}', [ConferenceController::class, 'show']);
 Route::put('/conferences/{id}', [ConferenceController::class, 'update']);
 Route::delete('/conferences/{id}', [ConferenceController::class, 'destroy']);
 Route::post('/conferences', [ConferenceController::class, 'store']);
 
+Route::get('/reports', [ReportController::class, 'index']);
+Route::get('/reports/{id}', [ReportController::class, 'show']);
+Route::put('/reports/{id}', [ReportController::class, 'update']);
+Route::delete('/reports/{id}', [ReportController::class, 'destroy']);
+Route::post('/reports', [ReportController::class, 'store']);
+
 //Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
 });
 Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
-     return $request->user();
+     return Auth::user();
 });
